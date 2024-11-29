1faaaa0f027cc3c3309f1f35f5ad22304a09008b
AbhinavGalar <abhinavgalar442@gmail.com>
2024-11-29 23:11:31 +0530

    update

---
+++ november 29/script.js
@@ -0,0 +1,16 @@
+let name1=document.getElementById("demo");
+let name2=document.getElementById("demo1");
+let name3=document.getElementById("demo2");
+
+
+
+
+function data(){
+    var name="abhinav";
+    var age= 19;
+    console.log(name+" "+age);
+    alert(name+" "+age);
+    document.getElementById("demo").innerHTML=name;
+}
+data();
+
