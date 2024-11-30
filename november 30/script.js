4db1388fa0ef4aa9f721f3102e88e58b367d6f8e
AbhinavGalar <abhinavgalar442@gmail.com>
2024-11-30 21:24:09 +0530

    update

---
+++ november 30/script.js
@@ -0,0 +1,20 @@
+// var para="hello";
+// var name ="abhinav";
+// var data=`this
+// is
+// a
+// string`;
+// let h1=document.getElementById("demo");
+// let div=document.getElementById("trial");
+// h1.innerHTML=data;
+// h1.innerHTML=`this is the answer" ${para} ${data} ${name}`;
+// div.innerHTML= `<h1>${name}</h1>`;
+// ===template literals /or string literals====
+
+function about(){
+   var name="abhinav ";
+   var city ="bhopal" ;
+var head=document.getElementById("demo");
+head.innerHTML=`<h1> ${name} ${city}  </h1>`;
+}
+about();
