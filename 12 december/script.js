e1a7db47a5c47c3ec2c362b21b0f63252dccda19
AbhinavGalar <abhinavgalar442@gmail.com>
2024-12-12 22:15:04 +0530

    update

---
+++ 12 december/script.js
@@ -0,0 +1,13 @@
+let mydata = "Google is a great place to work google offers great services ang goes many employees many amenities to the employees";
+// i is for case-sensitive and g is for global search
+let newData= mydata.replace(/google/ig, "amazon");
+console.log(newData);      
+// dom ,bom
+// es6 features 
+// var let const
+// functions 
+// object 
+// array
+// math method
+// string method
+   
