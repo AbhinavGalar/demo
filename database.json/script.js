f712f9133c9008565e2a2f50b12f6b9c64c6d7e0
AbhinavGalar <abhinavgalar442@gmail.com>
2025-01-04 15:30:22 +0530

    jan4

---
+++ database.json/script.js
@@ -0,0 +1,24 @@
+async function fet (params) {
+    let table = document.querySelector('#displaydata')
+    let res = await fetch  ("http://localhost:3000/emp")
+    let data = await res.json()
+    let finaldata = data.map((e)=>`
+     
+
+    <tr>
+    <td> ${e.id}</td>
+    <td> ${e.name}</td>
+    <td> ${e.age}</td>
+    <td> ${e.contact}</td>
+    <td> ${e.city}</td>
+    <td><button onclick ="mydelete
+</tr>
+    
+    `).join("")
+    table.innerHTML = finaldata
+}
+fet()
+
+function mydelete (id){
+    console.log(id)
+}
